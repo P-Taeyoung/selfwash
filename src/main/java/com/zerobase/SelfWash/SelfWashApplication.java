0bfa2850106cda6e53d25cb3a1957254d9bccda6
park <pty10510@gmail.com>
2025-02-14 20:35:57 +0900

    Initial commit

---
+++ src/main/java/com/zerobase/SelfWash/SelfWashApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.SelfWash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfWashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfWashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerobase/SelfWash/SelfWashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.SelfWash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfWashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
