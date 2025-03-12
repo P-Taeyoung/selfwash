51e8a8eeb2c0d5d307159ee46f37f55033eaed24
park <pty10510@gmail.com>
2025-03-12 20:01:00 +0900

    feat: 피드백 반영(문의 내역 데이터 가공 시 비교를 통해 String으로 포맷하는 로직 제거)

--- src/main/java/com/zerobase/SelfWash/owner_inquiry/domain/dto/OwnerInquiryDto.java
+++ src/main/java/com/zerobase/SelfWash/owner_inquiry/domain/dto/OwnerInquiryDto.java
@@ -15,6 +15,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.cglib.core.Local;
 
 @Builder
 @Setter
@@ -28,25 +29,26 @@ public class OwnerInquiryDto {
   private Long ownerId;
   private InquiryType inquiryType;
   private String contents;
-  private String resolvedTime;
+  private LocalDateTime resolvedTime;
 
   public static OwnerInquiryDto from(OwnerInquiry inquiry) {
-    OwnerInquiryDto inquiryDto = OwnerInquiryDto.builder()
-                                    .inquiryId(inquiry.getId())
-                                    .ownerId(inquiry.getOwnerId())
-                                    .inquiryType(inquiry.getInquiryType())
-                                    .contents(inquiry.getContents())
-                                    .build();
+    return OwnerInquiryDto.builder()
+        .inquiryId(inquiry.getId())
+        .ownerId(inquiry.getOwnerId())
+        .inquiryType(inquiry.getInquiryType())
+        .contents(inquiry.getContents())
+        .resolvedTime(inquiry.getResolvedTime())
+        .build();
 
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 문의처리완료");
-    try {
-      inquiryDto.setResolvedTime(inquiry.getResolvedTime().format(formatter));
-    } catch (NullPointerException e) {
-      inquiryDto.setResolvedTime("문의 미처리");
-    } catch (DateTimeParseException e) {
-      throw new RuntimeException(e + " : 유효한 시간이 아닙니다.");
-    }
-    return inquiryDto;
+//    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 문의처리완료");
+//    try {
+//      inquiryDto.setResolvedTime(inquiry.getResolvedTime().format(formatter));
+//    } catch (NullPointerException e) {
+//      inquiryDto.setResolvedTime("문의 미처리");
+//    } catch (DateTimeParseException e) {
+//      throw new RuntimeException(e + " : 유효한 시간이 아닙니다.");
+//    }
+//    return inquiryDto;
   }
 
 
